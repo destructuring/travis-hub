0c22e50d6fba8924abc7ce69a1c46e7767eeead7
Sven Fuchs <me@svenfuchs.com>
2013-08-12 17:53:35 +0200

    require jruby-openssl in bin/hub

--- lib/travis/hub.rb
+++ lib/travis/hub.rb
@@ -7,7 +7,6 @@ require 'travis/support/amqp'
 require 'travis/hub/queue'
 require 'travis/hub/error'
 require 'core_ext/kernel/run_periodically'
-require 'jruby-openssl'
 require 'raven'
 
 $stdout.sync = true
